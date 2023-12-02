72533390eb9386ad6eabb3d1f99ab6e8dedf9351
Noddek <4deeplearning@gmail.com>
2023-12-02 08:10:12 +0300

    Task 4.1.5 is complete.

--- Program.cs
+++ Program.cs
@@ -21,13 +21,16 @@
             Console.WriteLine("\tIs it winter? - {0}.", isWinter);
 
             Console.WriteLine("\nTask 4.1.4. Comparing variables of string type.");
-            string A = "";
-            string B = "";
-            bool C = (A != B); // The outer brackets can be removed
-            Console.WriteLine("\tIsn't A equal to B? - {0}.", C);
+            string A1 = "";
+            string B1 = "";
+            bool C1 = (A1 != B1); // The outer brackets can be removed
+            Console.WriteLine("\tIsn't A equal to B? - {0}.", C1);
 
-
-
+            Console.WriteLine("\nTask 4.1.5. Comparing variables of numeric types.");
+            int A2 = 1, B2 = 2;
+            double X2 = 3, Y2 = 4;
+            bool C2 = (A2 < B2) || (X2 > Y2);
+            Console.WriteLine("\tIs the condition met? - {0}.", C2);
 
 
             Console.ReadKey();
