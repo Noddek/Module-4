3cf51566bc496a9a2a487d1414a8f9e25fa21e3f
Noddek <4deeplearning@gmail.com>
2023-12-02 04:36:38 +0300

    Added examples with fruits and winter.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Comparing fruits:");
+            var myapples = 5;
+            var hisapples = 6;
+            var hispeares = 5;
+            var result = (myapples != hispeares) & (myapples < hisapples);
+            Console.WriteLine("\t" + result);
+
+            Console.WriteLine("\nFinding out if it's winter or not:");
+            int currentDayInYear = 293;
+            bool isLeapYear = false;
+            bool isWinter = 
+                (!isLeapYear && (currentDayInYear >= 335 || currentDayInYear <= 59)) // The outer brackets can be removed
+                ||
+                (isLeapYear && (currentDayInYear >= 336 || currentDayInYear <= 60)); // The outer brackets can be removed
+            Console.WriteLine("\tIs it winter? - {0}.", isWinter);
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
