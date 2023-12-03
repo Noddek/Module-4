7ff14d4797a5ddd607e9f12cff4b20fed90c91c5
Noddek <4deeplearning@gmail.com>
2023-12-03 16:25:07 +0300

    Module 4.2 is started. Added many examples with cycles. Task 4.2.13 is complete.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.WebSockets;
+﻿using System.Diagnostics.Metrics;
+using System.Net.WebSockets;
 using static System.Collections.Specialized.BitVector32;
 
 namespace BasicConstructions
@@ -7,7 +8,8 @@ namespace BasicConstructions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Comparing fruits:");
+            Console.WriteLine("MODULE 4.1. Selection statements (conditional statements).");
+            Console.WriteLine("\nComparing fruits:");
             var myapples = 5;
             var hisapples = 6;
             var hispeares = 5;
@@ -124,7 +126,8 @@ namespace BasicConstructions
             Console.WriteLine("\tResult of ternary operation: " + c3);
 
 
-            Console.WriteLine("\nTask 4.1.17. A program about colors to reinforce skills.");
+            Console.WriteLine("\nTask 4.1.17. A program about colors to reinforce skills. [COMMENTED]");
+            /*
             Console.Write("\tPlease write your favorite color with a small letter: ");
             var color = Console.ReadLine();
             if (color == "red")
@@ -145,9 +148,11 @@ namespace BasicConstructions
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("\tOK, your color is cyan!");
             }
+            */
 
             Console.WriteLine("\nSelection statement (conditional statement): switch.");
-            Console.WriteLine("\nTask 4.1.18. Color selection with switch.");
+            Console.WriteLine("\nTask 4.1.18. Color selection with switch. [COMMENTED]");
+            /*
             Console.Write("\tPlease write your favorite color with a small letter: ");
             var color1 = Console.ReadLine();
             switch (color1)
@@ -180,12 +185,320 @@ namespace BasicConstructions
                     Console.WriteLine("\tOK, your colors are yellow and red!");
                     break;
             }
+            */
+            
+            Console.WriteLine("\n\nMODULE 4.2. Iteration statements (cycles).");
+            Console.WriteLine("\nColor change cycle. [COMMENTED.]");
+            /*
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("\tIteration {0}", i);
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+            }
+            */
+
+            Console.WriteLine("\nCycle with double i:");
+            for (double i = 0.0; i < 0.1; i += 0.01)
+            {
+                Console.WriteLine("\ti = " + i);
+            }
+            
+            Console.WriteLine("\nCycle with long i:");
+            for (long i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\ti = " + i);
+            }
+
+            Console.WriteLine("\nCycle with an external variable j:");
+            int j = 25;
+            for (; j < 30; j++)
+            {
+                Console.WriteLine("\tj = " + j);
+            }
+
+            Console.WriteLine("\nCycle with counter change in the cycle body:");
+            for (int i = 0; i < 5; )
+            {
+                Console.WriteLine("\ti = " + i);
+                i++;
+            }
+
+            Console.WriteLine("\nCycle in reverse order:");
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine("\ti = " + i);
+            }
+
+            Console.WriteLine("\nInfinite cycle for: [COMMENTED]");
+            /*
+            for (int i = 1; ; i++)
+            {
+                Console.WriteLine("\tIteration " + i);
+                Console.Write("\tWrite a word: ");
+                string word = Console.ReadLine();
+                Console.WriteLine("\tYour word: " + word);
+            }
+            */
+
+            Console.WriteLine("\nInfinite cycle 'for' for changing colors: [COMMENTED]");
+            /*
+            for (; ;)
+            {
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+            }
+            */
+
+            Console.WriteLine("\nInfinite cycle 'while' for changing colors: [COMMENTED]");
+            /*
+            while (true)
+            {
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+            }
+            */
+
+            Console.WriteLine("\nInfinite cycle 'do...while' for changing colors: [COMMENTED]");
+            /*
+            do
+            {
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+            }
+            while (true);
+            */
+
+
+            Console.WriteLine("\nCycle 'while' with counter for changing colors: [COMMENTED]");
+            /*
+            int k = 0;
+            while (k < 3)
+            {
+                Console.WriteLine("\tIteration " + k);
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+                k++;
+            }
+            */
+
+            Console.WriteLine("\nTask 4.2.11. Cycle 'do...while' with counter for changing colors: [COMMENTED]");
+            /*
+            int t = 1;
+            do
+            {
+                Console.WriteLine("\tIteration " + t);
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+                t++;
+            }
+            while (t < 4);
+            */
+
+            Console.WriteLine("\nTask 4.2.13. Difference between 'while' and 'do...while'.");
+            Console.WriteLine("\tCycle 'while':");
+            int k1 = 0;
+            while (k1 < 0)
+            {
+                Console.WriteLine("\tIteration " + k1);
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+                k1++;
+            }
+            Console.WriteLine("\tCycle 'do...while':");
+            int t1 = 0;
+            do
+            {
+                Console.WriteLine("\tIteration " + t1);
+                Console.Write("\tPlease write your favorite color with a small letter: ");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("\tOK, your color is yellow!");
+                        break;
+                }
+                t1++;
+            }
+            while (t1 < 0);
 
 
 
 
-
-
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
             Console.ReadKey();
 
         }
